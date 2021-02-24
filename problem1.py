e54a7f3a4777284a9a451d1a0d14da15d5e63674
KevinWang1122 <yukai.wang23@ycdsbk12.ca>
2021-02-24 14:18:03 +0000

    Finished LiveHack #2, make up from yesterday since I was not present.

--- problem1.py
+++ problem1.py
@@ -1,1 +1,17 @@
+eyes = int(input("How many eyes? "))
+antennas = int(input("How many antennas? "))
 
+if antennas >= 3 and eyes <= 4:
+  print("Life form detected: AudreyMartian")
+
+elif antennas == 0 and eyes == 2:
+  print("Life form detected: MattDamonMartian")
+
+elif antennas <= 6 and eyes >= 2:
+  print("Life form detected: MaxMartian")
+
+  if antennas <= 2 and eyes <= 3:
+      print("Life form detected: BrooklynMartian")
+      
+else:
+  print("No life form detected.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,9 @@
+side_1 = int(input("Please enter the value of side 1: "))
+side_2 = int(input("Please enter the value of side 2: "))
+side_3 = int(input("Please enter the value of side 3: "))
 
+if (side_1 + side_2 > side_3) and (side_2 + side_3 > side_1) and (side_1 + side_3 > side_2) :
+  print("This is a triangle.")
+else:
+  print("This is not a triangle.")
+    
