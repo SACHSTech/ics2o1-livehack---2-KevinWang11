6b8141a15325d9b6edf94505b37093ce7bdf7d95
KevinWang1122 <yukai.wang23@ycdsbk12.ca>
2021-02-24 14:27:01 +0000

    Added comments and headings.

--- problem1.py
+++ problem1.py
@@ -1,6 +1,18 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem1.py
+Purpose:  Create a program that allows NASA's perseverance rover to determine what life form it is looking at based on the number of eyes and antennas it has. 
+
+Author: Wang.K
+ 
+Created:  24/02/2021
+------------------------------------------------------------------------------
+"""
+#asks the user for the number of eyes and antennas.
 eyes = int(input("How many eyes? "))
 antennas = int(input("How many antennas? "))
 
+#calculates what life form the user specified eyes and antennas will be. 
 if antennas >= 3 and eyes <= 4:
   print("Life form detected: AudreyMartian")
 
@@ -12,6 +24,7 @@ elif antennas <= 6 and eyes >= 2:
 
   if antennas <= 2 and eyes <= 3:
       print("Life form detected: BrooklynMartian")
-      
+
+#prints error if the user input is not any of the Martians specified in the question.   
 else:
   print("No life form detected.")
--- problem2.py
+++ problem2.py
@@ -1,9 +1,23 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem2.py
+Purpose:  Write a program to have the user enter three lengths of sides and determine whether the figure is a triangle or not. . 
+
+Author: Wang.K
+ 
+Created:  24/02/2021
+------------------------------------------------------------------------------
+"""
+# asks the user for the three sides of the triangle
 side_1 = int(input("Please enter the value of side 1: "))
 side_2 = int(input("Please enter the value of side 2: "))
 side_3 = int(input("Please enter the value of side 3: "))
 
+# calculates if the sides can form a triangle
 if (side_1 + side_2 > side_3) and (side_2 + side_3 > side_1) and (side_1 + side_3 > side_2) :
   print("This is a triangle.")
+
+# if the above is false, then the three sides cannot form a triangle.
 else:
   print("This is not a triangle.")
     
